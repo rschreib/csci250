ffc942e39796dc6f87cc888ae62613cb12283eda
rschreib <rschreib@mymail.mines.edu>
2018-01-23 15:13:20 -0700

    new assignment

---
+++ Python Practice.py
@@ -0,0 +1,27 @@
+# Robert Schreibman
+# CSCI250 - Sensors
+# 01-23-18
+
+# Practicing Python Code
+
+import numpy as np
+import time as tm
+import random as rn
+
+mystring = "helloWorld"
+myinteger = 18
+myfloat = 3.37
+
+print("String:\t",mystring, "\nInteger:", myinteger, "\nFloat:\t", myfloat, "\n")
+myfloat = rn.uniform(1,100)
+
+tm.sleep(5)
+
+print("String:\t",mystring, "\nInteger:", myinteger, "\nFloat:\t", myfloat, "\n")
+
+myinteger*myfloat
+
+tm.sleep(5)
+
+print("String:\t",mystring, "\nInteger:", myinteger, "\nFloat:\t", myfloat, "\n")
+tm.sleep(5)
