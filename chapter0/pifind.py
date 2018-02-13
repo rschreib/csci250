68cc99e81729b5075da1e73c6cc8de950c48603d
rschreib <rschreib@mymail.mines.edu>
2018-02-13 13:58:45 -0700

    auto commit

--- chapter0/pifind.py
+++ chapter0/pifind.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python2.7
 import smtplib, string, subprocess
+import os
+
+os.system(" hostname -I | awk '{print $1;}' > .ipaddress.txt")
 # Script to get IP Address while running a Raspberry Pi headless.
 # pifind.py gets the system parameters you want to know and 
 # emails them through gmail to a destination of your choice
@@ -20,15 +23,17 @@ username = 'robert.schreibman' #'<your username for the gmail account without @g
 password = 'imnotsure18' #'<the password for that account>'  
 
 output_if = subprocess.Popen(['ifconfig'], stdout=subprocess.PIPE).communicate()[0]
-output_cpu = open('/proc/cpuinfo', 'r').read()
+#output_cpu = open('/proc/cpuinfo', 'r').read()
+output_ip = open('.ipaddress.txt', 'r').read()
 
 BODY = string.join((
         "From: %s" % fromaddr,
         "To: %s" % toaddr,
-        "Subject: Your RasPi just booted",
+        #"Subject: Your RasPi just booted",
+        "Subject: RasPi {}".format(output_ip),
         "",
         output_if,
-        output_cpu,
+        #output_cpu,
         ), "\r\n")
       
 # send the email  
