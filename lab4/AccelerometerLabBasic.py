5750c5573641807ac74975be709c7b9153d91bd3
rschreib <rschreib@mymail.mines.edu>
2018-02-22 14:45:20 -0700

    auto commit

--- lab4/AccelerometerLabBasic.py
+++ lab4/AccelerometerLabBasic.py
@@ -44,19 +44,19 @@ try:
 		
 		numberOfBits = 16
 		xAccl = (MSB_x * 256 + LSB_x) / numberOfBits
+		yAccl = (MSB_y * 256 + LSB_y) / numberOfBits
+		zAccl = (MSB_z * 256 + LSB_z) / numberOfBits
 		if xAccl > 2047:
 			xAccl -= 4096
-		yAccl = (MSB_y * 256 + LSB_y) / numberOfBits
+
 		if yAccl > 2047:
 			yAccl -= 4096	
-		zAccl = (MSB_z * 256 + LSB_z) / numberOfBits
+
 		if zAccl > 2047:
 			zAccl -= 4096
 		print("x: ",xAccl)
 		print("y: ",yAccel)
 		print("z: ",zAccel)
-		
-			
 			
 #capture the control c and exit cleanly
 except(KeyboardInterrupt, SystemExit): 
