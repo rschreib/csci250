f01f92b4163a254a571ff510b03e8f1c0d2f90be
rschreib <rschreib@mymail.mines.edu>
2018-02-22 14:58:22 -0700

    auto commit

---
+++ lab4/AccelerometerLabBasic.py
@@ -0,0 +1,38 @@
+#Name: Wendy Fisher (TA: Benjamin Tarman)
+#Date: Feb 22, 2018
+#Description: This sample file is for Lab4 - and intended as a bare-bones structure to
+#       work with the I2C and the accelerometer. Once the device is wired and RPi
+#       running, this code should work as is. Students are tasked to modify this
+#       code to an object-oriented solution for credit.
+import smbus 
+import time
+
+# Get I2C bus - initial bus to channel 1
+bus = smbus.SMBus(1) 
+
+try:
+    while True:
+        #Parameters for write_byte_data
+        #1. Address of the device
+        #2. Communication data - active mode control register
+        #3. Our data - 0 (standby mode) or 1 (active)
+        bus.write_byte_data(0x1D, 0x2A, 1) 
+        time.sleep(0.5)
+
+        #Read from the status register, real-time status register 0x00
+        #Data returned will be an array
+        #Contents of 7 bytes read and stored in data array represent:
+        #status (ignore), MSBx, LSBx, MSBy, LSBy, MSBz, LSBz
+        data = bus.read_i2c_block_data(0x1D, 0x00, 7)     
+
+        #put register in standbye mode
+        bus.write_byte_data(0x1D, 0x2A, 0) 
+        time.sleep(0.5)
+
+        print(data)
+
+#capture the control c and exit cleanly
+except(KeyboardInterrupt, SystemExit): 
+    print("User requested exit... bye!")
+    
+#Sample modified from https://www.controleverything.com/content/Accelorometer?sku=MMA8452Q_I2CS#tabs-0-product_tabset-2
