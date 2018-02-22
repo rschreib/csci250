562bf9d4db1f3d218d480482c4cba4abea302139
rschreib <rschreib@gmymail.mines.edu>
2018-02-21 19:15:40 -0700

    auto commit

--- L9.py
+++ L9.py
@@ -5,12 +5,27 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import operator
 
 data = np.loadtxt("data.csv", delimiter=",")
-#print(data[1])
 
-x = data[0]
-y = data[1]
+x = data[0] #time
+y = data[1] #cheese
 
-plt.plot(x,y)
-#plt.show()
+plt.plot(x,y,'rp')
+plt.xlabel("Time (years)")
+plt.ylabel("Cheese Consumption (lbs)")
+plt.title("Cheese Consumption in the Last Decade")
+
+plt.savefig('plot.jpg')
+
+plt.show()
+
+d = {}
+for key, value in zip(y,x):
+    d[key] = value
+
+print(d)
+
+for cheese in sorted(d, reverse=True):
+    print("In {}, {} lbs of cheese was consumed!".format(d[cheese],cheese))
