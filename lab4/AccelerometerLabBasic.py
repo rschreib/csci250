9194f506a779ea9cc19e027ad31c22f3c43dd224
rschreib <rschreib@mymail.mines.edu>
2018-03-04 01:35:04 -0700

    auto commit

--- lab4/AccelerometerLabBasic.py
+++ lab4/AccelerometerLabBasic.py
@@ -74,9 +74,9 @@ def pulse(sleepTime):
 def pulse_rate(x,y,z):
     magnitude = math.sqrt(x*x + y*y + z*z)
     if (magnitude > 100):
-        pulse(0.4)
-    elif (magnitude > 300):
         pulse(0.1)
+    elif (magnitude > 300):
+        pulse(0.01)
     else:
         pass
 
