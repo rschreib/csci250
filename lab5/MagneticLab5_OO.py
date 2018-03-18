14b13dc134ed4e312fcb6a3661a7bae8756ecf73
rschreib <rschreib@mymail.mines.edu>
2018-03-18 11:32:28 -0600

    auto commit

--- lab5/MagneticLab5_OO.py
+++ lab5/MagneticLab5_OO.py
@@ -47,7 +47,7 @@ GPIO.setup(LED, GPIO.OUT)
 #area for the logic to detect high/low from reed switch and light LED
 try:
     speedometer = Speedometer()
-    readSensor = GPIO.add_event_detect(sensor,GPIO.FALLING,callback=speedometer(), bouncetime = 25)
+    readSensor = GPIO.add_event_detect(sensor,GPIO.FALLING,callback=speedometer(sensor), bouncetime = 25)
     while True:
       #capture and print the input from the reed switch using GPIO.input
       # readSensor = GPIO.input(sensor)
